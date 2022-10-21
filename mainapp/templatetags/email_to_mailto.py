c4b6ce8f69e2cfca76435216d37fc05e3daff54c
ssergo99 <ssergo99.git@gmail.com>
2022-10-21 08:10:12 +0000

    DZ3

---
+++ mainapp/templatetags/email_to_mailto.py
@@ -0,0 +1,6 @@
+from django import template
+from django.utils.safestring import mark_safe
+register = template.Library()
+@register.filter
+def email_to_mailto(value):
+    return mark_safe(f"<a href='mailto:{value}'>{value}</a>")
